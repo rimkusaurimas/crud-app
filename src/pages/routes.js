b24a3db9daadd83da90343d3d26918da7c6d30a8
Aurimas Rimkus <labas@aurimasrimkus.lt>
2022-01-08 00:27:53 +0200

    Add more routes, main navigation init

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -2,15 +2,19 @@ import React from "react";
 import { MainPage } from "./MainPage";
 import { Home } from "./Home";
 import { NotFound } from "./NotFound";
+import { AddUser } from "./AddUser";
+import { EditUser } from "./EditUser";
 
-export const getRoutes = () => {
-  return [
-    {
-      path: "/",
-      element: <Home />,
-      name: "home",
-      children: [{ index: true, element: <MainPage />, name: "main-page" }],
-    },
-    { path: "/*", element: <NotFound /> },
-  ];
-};
+export const getRoutes = () => [
+  {
+    path: "/",
+    element: <Home />,
+    name: "home",
+    children: [
+      { index: true, element: <MainPage />, name: "main-page" },
+      { path: "/add-user", element: <AddUser />, name: "add-user" },
+      {path: "edit-user", element: <EditUser />, name: "edit-user"},
+    ],
+  },
+  { path: "/*", element: <NotFound /> },
+];
