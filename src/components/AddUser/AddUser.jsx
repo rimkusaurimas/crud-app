4dbc6bc9f162e56df87cb82bc117827083e85297
Aurimas Rimkus <labas@aurimasrimkus.lt>
2022-01-08 03:19:34 +0200

    Implement submit form validation

--- src/components/AddUser/AddUser.jsx
+++ src/components/AddUser/AddUser.jsx
@@ -1,23 +1,81 @@
-import React from "react";
-import { Button, InputGroup, FormControl } from "react-bootstrap";
+import { React, useState } from "react";
+import { Button, InputGroup, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styles from "./add-user.module.scss";
 
 export const AddUser = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <section className={styles.addUser}>
-      <InputGroup className="mb-3">
-        <InputGroup.Text className={styles.addUserFieldText}>First name</InputGroup.Text>
-        <FormControl aria-label="First name" required />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <InputGroup.Text className={styles.addUserFieldText}>Last name</InputGroup.Text>
-        <FormControl aria-label="Last name" required />
-      </InputGroup>
-      <InputGroup>
-        <InputGroup.Text className={styles.addUserFieldText}>Email</InputGroup.Text>
-        <FormControl aria-label="Email" />
-      </InputGroup>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <InputGroup className="mb-3">
+          <InputGroup.Text className={styles.addUserFieldText}>
+            First name
+          </InputGroup.Text>
+          <Form.Control placeholder="John" aria-label="First name" required />
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Text className={styles.addUserFieldText}>
+            Last name
+          </InputGroup.Text>
+          <Form.Control placeholder="Doe" aria-label="Last name" required />
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Text className={styles.addUserFieldText}>
+            Address
+          </InputGroup.Text>
+          <Form.Control
+            placeholder="Somewhere st. 1 , USA"
+            aria-label="Address"
+            required
+          />
+        </InputGroup>
+        <InputGroup>
+          <InputGroup.Text className={styles.addUserFieldText}>
+            Email
+          </InputGroup.Text>
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
+            aria-label="Email"
+            required
+          />
+        </InputGroup>
+        <div className={styles.addUserCountry}>
+          <Form.Label className={styles.addUserCountryTitle} htmlFor="country">
+            Country
+          </Form.Label>
+          <Form.Select
+            className={styles.addUserCountrySelect}
+            id="country"
+            aria-label="Country"
+            required
+          >
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+        </div>
+        <Button className={styles.addUserButton} type="submit">Add user</Button>
+      </Form>
+      <div>
+        <Link to={"/"}>
+          <Button className={styles.addUserCancelButton} variant="dark">
+            cancel
+          </Button>
+        </Link>
+      </div>
     </section>
   );
 };
